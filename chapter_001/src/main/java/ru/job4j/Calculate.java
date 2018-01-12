ee9e02b3dc1ea07299e87bd5d62f663c84404d8c
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-12 12:17:14 +0600

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+* class Calculate. Выводит строку Hello world! в консоль. Решение задания #141.
+* @author Viatcheslav Bugakov
+* @since 12.01.2018
+*/
+
 public class Calculate{
     public static void main (String[] args){
         System.out.println("Hello, world!");
