f18b99f9dcaabc087fa69ffe22235dd058d5200a
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-14 19:48:26 +0600

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -7,7 +7,21 @@ package ru.job4j;
 */
 
 public class Calculate{
+    /**
+     * Method main.
+     * @param String[] args.
+     */
+    
     public static void main (String[] args){
         System.out.println("Hello, world!");
+    }
+    
+     /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo(three times) and your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test for methods of class Calculate. Решение задания #4122.
+* @author Viatcheslav Bugakov
+* @version $id$
+* @since 0.1
+*/
+
+public class CalculateTest{
+    /**
+    * Test method echo.
+    */
+    
+    @Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Viatcheslav Bugakov";
+    String expect = "Echo, echo, echo : Viatcheslav Bugakov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+}
