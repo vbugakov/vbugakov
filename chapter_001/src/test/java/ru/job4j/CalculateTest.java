101fe1be2fc2755d950792a938444ae2cf072eec
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-18 20:02:37 +0600

    Clean up non used imports

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,8 +1,6 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
