df3e60313cc8f6ab9cc745750cc4c76a14d32d7b
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-19 11:53:26 +0600

    2.4. Расстояние между точками в системе координат. [#188] Stage: Creation structure of package Class Point: - Creation fields and Constructor.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+/**
+ * Class Point - Realisation of Point in coordinate system with
+ * capability of calculate distance to himself
+ * from task 2.4. Расстояние между точками в системе координат. [#188]
+ * @author Viatcheslav Bugakov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    /**
+     * Constructor.
+     * @author Viatcheslav Bugakov
+     * @param x
+     * @param y
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
