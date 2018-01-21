07e9c5614d094ef954c5230af8dc78d21636715f
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-21 21:27:44 +0600

    3.1. Глупый бот. [#31323] Class DummyBot: - copy paste from task description. - Change some condition and answers. Class DummyBotTest: - copy paste from task description with no changes. mvm validate done. mvn test done.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+/**
+ * Class DummyBot solution
+ * for task 3.1. Глупый бот. [#31323]
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @author Viatcheslav Bugakov
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
