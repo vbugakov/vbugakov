e872d1b3ef291dc9be1b019fd295817bacd6c9a8
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-23 17:04:21 +0600

    4.3. Построить шахматную доску в псевдографике. [#13559] "Copy paste & javadoc" stage.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,43 @@
+package ru.job4j.loop;
+
+/**
+ * Class Board - solution of task :
+ * 4.3 Построить шахматную доску в псевдографике. [#13559]
+ * @author Viatcheslav Bugakov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Board {
+    /**
+     * method paint .
+     * @param width - width of board
+     * @param height - height of board
+     * @return string of board painted by chars:
+     * example size 3x3:
+     * x x
+     *  x
+     * x x
+     */
+
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+
+        for ( ) {
+            for ( ) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if () {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+                // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.loop;
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class BoardTest - for testing methods of class Board
+ * task 4.3. Построить шахматную доску в псевдографике. [#13559]
+ * @author Viatcheslav Bugakov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class BoardTest {
+
+    /**
+     * Test of method paint .
+     * Should return string of board painted by chars:
+     * size 3x3
+     * x x
+     *  x
+     * x x
+     */
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+    /**
+     * Test of method paint .
+     * Should return string of board painted by chars:
+     * size 3x3
+     * x x x
+     *  x x
+     * x x x
+     *  x x
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        //напишите здесь тест, проверяющий формирование доски 5 на 4.
+    }
+}
