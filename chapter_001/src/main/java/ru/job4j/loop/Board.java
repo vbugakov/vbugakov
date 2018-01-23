2a44c81f4192d86af6b126dca8a9ded43d5a1a71
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-23 17:25:16 +0600

    4.3. Построить шахматную доску в псевдографике. [#13559] "Green Stage" all tests passed mvn validate done mvn test done

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -23,12 +23,12 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-/*
-        for ( ) {
-            for ( ) {
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if () {
+                if (((i % 2 == 0) && (j % 2 == 0)) || ((i % 2 != 0) && (j % 2 != 0))) {
                     screen.append("X");
                 } else {
                     screen.append(" ");
@@ -37,7 +37,7 @@ public class Board {
                 // добавляем перевод на новую строку.
             screen.append(ln);
         }
-  */
+
         return screen.toString();
     }
 
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -28,13 +28,13 @@ public class BoardTest {
         Board board = new Board();
         String result = board.paint(3, 3);
         final String line = System.getProperty("line.separator");
-        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        String expected = String.format("X X%s X %sX X%s", line, line, line);
         assertThat(result, is(expected));
     }
     /**
      * Test of method paint .
      * Should return string of board painted by chars:
-     * size 3x3
+     * size 5x4
      * x x x
      *  x x
      * x x x
@@ -46,7 +46,7 @@ public class BoardTest {
         Board board = new Board();
         String result = board.paint(5, 4);
         final String line = System.getProperty("line.separator");
-        String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line);
         assertThat(result, is(expected));
     }
 }
