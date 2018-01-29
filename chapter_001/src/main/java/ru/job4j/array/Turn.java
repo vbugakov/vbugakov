81996c4ebebce0f1ad74ceb793b5461a97c66919
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-29 16:25:49 +0600

    5.2. Перевернуть массив. [#4441] "Frame stage" Creation of classes frames and docs.

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ * Class Turn -  solution of task:
+ * 5.2. Перевернуть массив. [#4441]
+ * @author Viatcheslav Bugakov
+ * @version %Id%
+ * @since 0.1
+ */
+public class Turn {
+    /**
+     * method indexOf
+     * from 1 to bound
+     * @param array -  array for turn
+     * @return turned array.
+     */
+    public int[] back(int[] array){
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class TurnTest - for testing methods of class Turn
+ * task 5.2. Перевернуть массив. [#4441]
+ * @author Viatcheslav Bugakov
+ * @version $Id$
+ * @since 0.1
+ */
+public class TurnTest {
+
+    /**
+     * Test for method back.
+     * return turned array
+     */
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        //напишите здесь тест, проверяющий переворот массива с чётным числом элементов, например {2, 6, 1, 4}.
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+    }
+}
