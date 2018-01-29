50bee86a30572d2379f25999900e9cbe6dce60e1
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-01-29 17:15:32 +0600

    5.2. Перевернуть массив. [#4441] "Green stage" method back realized. mvn validate is done mvn test is done

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -14,7 +14,13 @@ public class Turn {
      * @param array -  array for turn
      * @return turned array.
      */
-    public int[] back(int[] array){
+    public int[] back(int[] array) {
+        int buffer;
+            for (int i = 0; i < array.length / 2; i++) {
+                buffer = array[i];
+                array[i] = array[array.length - 1 - i];
+                array[array.length - 1 - i] = buffer;
+            }
         return array;
     }
 }
