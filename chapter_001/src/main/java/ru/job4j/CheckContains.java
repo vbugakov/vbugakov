c0cd73fa9b735029d596a3963c8ba1cce4ae141e
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-02-04 17:17:15 +0600

    Проверка, что одно слово находится в другом слове [#228] creation of frames "Red Stage" Tests done but not passed

---
+++ chapter_001/src/main/java/ru/job4j/CheckContains.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+/**
+ * Class CheckContains -  solution of task:
+ * Проверка, что одно слово находится в другом слове [#228]
+ * task description:
+ * Создать программу проверяющую, что словo String находится в другом слове String.
+ * В программе должен быть метод
+ * boolean contains(String origin, String sub).
+ * Нельзя использовать String.indexOf String.contains.
+ * Нужно преобразовать строку в массив символов и проверить.
+ *
+ * Пример.
+ *
+ * String origin = "Привет";
+ * String sub = "иве";
+ *
+ * boolean result = contains(origin, sub); - true - так как "иве" есть в слове "Привет".
+ *
+ * @author Viatcheslav Bugakov
+ * @version %Id%
+ * @since 0.1
+ */
+public class CheckContains {
+    /**
+     * method contains
+     * @param origin -  word for analysis.
+     * @param sub - word looking for
+     * @return true if origin contains sub.
+     */
+    boolean contains(String origin, String sub){
+        boolean result =false;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/CheckContainsTest.java
@@ -0,0 +1,37 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class CheckContainsTest - for testing methods of class CheckContains
+ * Проверка, что одно слово находится в другом слове [#228]
+ * @author Viatcheslav Bugakov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class CheckContainsTest {
+    private CheckContains check = new CheckContains();
+    /**
+     * Test for method contains.
+     * for origin = "Привет" and sub = "иве"
+     * should return true;
+     */
+    @Test
+    public void whenContainsThenTrue() {
+        assertThat(check.contains("Привет", "иве"), is(true));
+    }
+
+    /**
+     * Test for method contains.
+     * for origin = "Привет" and sub = "веи"
+     * should return false;
+     */
+    @Test
+    public void whenNoContainsThenFalse() {
+        assertThat(check.contains("Привет", "веи"), is(false));
+    }
+}
+
