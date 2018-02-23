148570e271cf21188016f3a4da2426bf7aca97ac
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-02-23 15:46:42 +0600

    Доп задание части 7. Соединить два отсортированных массива.[#42419] frame & red stage: Class MergeArrays: - Creation. - Creation method merge. Class MergeArraysTest: -Creation of tests. tEst created but not passed.

---
+++ chapter_001/src/main/java/ru/job4j/MergeArrays.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+/**
+ * Class MergeArrays -  solution of task:
+ * Доп задание. Соединить два отсортированных массива.[#42419]
+ * Task description:
+ * Обьединить два отсортированниых архива в один отсортированный
+ *
+ * @author Viathceslav Bugakov
+ * @version %Id%
+ * @since 0.1
+ */
+
+public class MergeArrays {
+    /**
+     * Merges two presorted arrays.
+     * @param leftArray - array for merge
+     * @param rightArray array for merge
+     * @return one merged sorted array.
+     */
+    public int[] merge(int[] leftArray, int[] rightArray) {
+        int[] resultArray = new int[leftArray.length + rightArray.length];
+
+        return resultArray;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/MergeArraysTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class MergeArraysTest - for testing methods of class MergeArays
+ * task: Доп задание. Соединить два отсортированных массива.[#42419]
+ *
+ * @author Viathceslav Bugakov
+ * @version %Id%
+ * @since 0.1
+ */
+
+public class MergeArraysTest {
+    /**
+     * test of merge method
+     * receive two array [1, 2] & [3, 4]
+     * returns [1, 2, 3, 4]
+     */
+    @Test
+    public void whenTwoPresortedThenOneMergedSorted() {
+        int [] result = new MergeArrays().merge(new int[] {1, 2}, new int[] {3, 4});
+        int [] expected = {1, 2, 3, 4};
+        assertThat(result, is(expected));
+    }
+}
