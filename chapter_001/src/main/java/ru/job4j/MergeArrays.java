504263d84f68c2a6b20e6b768c3ec162d166b0d1
Viatcheslav Bugakov <rogerkind@mail.ru>
2018-02-23 18:37:41 +0600

    Доп задание части 7. Соединить два отсортированных массива.[#42419] Green Stage: Class MergeArrays - Creation method merge.
    Test passed.

--- chapter_001/src/main/java/ru/job4j/MergeArrays.java
+++ chapter_001/src/main/java/ru/job4j/MergeArrays.java
@@ -20,6 +20,25 @@ public class MergeArrays {
      */
     public int[] merge(int[] leftArray, int[] rightArray) {
         int[] resultArray = new int[leftArray.length + rightArray.length];
+        int l=0;
+        int r=0;
+        for (int i = 0; i <resultArray.length; i++) {
+            if(l < leftArray.length && r <rightArray.length) {
+                if(leftArray[l] < rightArray[r]) {
+                    resultArray[i] = leftArray[l];
+                    l++;
+                } else {
+                    resultArray[i] = rightArray[r];
+                    r++;
+                }
+            } else if(l<leftArray.length) {
+                resultArray[i] = leftArray[l];
+                l++;
+            } else if(r<rightArray.length) {
+                resultArray[i] = rightArray[r];
+                r++;
+            }
+        }
 
         return resultArray;
     }
